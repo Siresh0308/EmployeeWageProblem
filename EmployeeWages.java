66f03f2e745ed78aa4a6fb2fc9e7ba44c12b40d1
Resh@308 <reshsingh1995@gmail.com>
2022-07-17 15:33:33 +0530

    Welcome message added to employe wage calculation

---
+++ EmployeeWages.java
@@ -0,0 +1,11 @@
+package EmployeewageProblem.bridgelabz.com;
+
+public class EmployeeWages {
+    public static void main(String[] args){
+
+        System.out.println("Wecome to Employee wage calculation");
+
+    
+}
+    
+}
